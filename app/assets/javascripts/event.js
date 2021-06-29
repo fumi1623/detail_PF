89a8626c01a711c0b9220b1b52277a58597f1981
fumi1623 <ancient17.hirofumi@gmail.com>
2021-06-29 13:58:59 +0000

    [preUpdate]event_index_side

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -7,49 +7,49 @@ $(function() {
 
 // indexページ
 
-$(function() {
-  $('.event-index--today__box').hide();
-});
+// $(function() {
+//   $('.event-index--today__box').hide();
+// });
 
-$(function() {
-  $('.event-index--tag__menu').hide();
-});
+// $(function() {
+//   $('.event-index--tag__menu').hide();
+// });
 
-$(function() {
-  $('.event-index--group__box').hide();
-});
+// $(function() {
+//   $('.event-index--group__box').hide();
+// });
 
-$(function() {
-  $('.event-index--invitation__box').hide();
-});
+// $(function() {
+//   $('.event-index--invitation__box').hide();
+// });
 
-$(function() {
-  $('.event-index--today__title').click(function() {
-    $('.today-events-icon').toggleClass('icon_rotate');
-    $('.event-index--today__box').slideToggle();
-  });
-});
+// $(function() {
+//   $('.event-index--today__title').click(function() {
+//     $('.today-events-icon').toggleClass('icon_rotate');
+//     $('.event-index--today__box').slideToggle();
+//   });
+// });
 
-$(function() {
-  $('.event-index--tag__title').click(function() {
-    $('.tag-index-icon').toggleClass('icon_rotate');
-    $('.event-index--tag__menu').slideToggle();
-  })
-})
+// $(function() {
+//   $('.event-index--tag__title').click(function() {
+//     $('.tag-index-icon').toggleClass('icon_rotate');
+//     $('.event-index--tag__menu').slideToggle();
+//   })
+// })
 
-$(function() {
-  $('.event-index--group__title').click(function() {
-    $('.group-index-icon').toggleClass('icon_rotate');
-    $('.event-index--group__box').slideToggle();
-  })
-})
+// $(function() {
+//   $('.event-index--group__title').click(function() {
+//     $('.group-index-icon').toggleClass('icon_rotate');
+//     $('.event-index--group__box').slideToggle();
+//   })
+// })
 
-$(function() {
-  $('.event-index--invitation__title').click(function() {
-    $('.invitation-index-icon').toggleClass('icon_rotate');
-    $('.event-index--invitation__box').slideToggle();
-  })
-})
+// $(function() {
+//   $('.event-index--invitation__title').click(function() {
+//     $('.invitation-index-icon').toggleClass('icon_rotate');
+//     $('.event-index--invitation__box').slideToggle();
+//   })
+// })
 
 // プリセット関連
 $(function() {
